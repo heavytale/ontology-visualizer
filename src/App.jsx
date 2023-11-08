9b1133ebe9863143d1511a895451dea31c38fcaf
heavytail-sv10 <k0s1k0s1k0@korea.ac.kr>
2023-11-08 13:20:12 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import React, { useCallback, useRef, useState } from 'react';
+import ReactFlow, {
+  addEdge,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+} from 'reactflow';
+
+import { nodes as initialNodes, edges as initialEdges } from './initial-elements';
+import CustomNode from './CustomNode';
+import ContextMenu from './ContextMenu';
+
+import 'reactflow/dist/style.css';
+import './overview.css';
+
+const nodeTypes = {
+  custom: CustomNode,
+};
+
+const minimapStyle = {
+  height: 120,
+};
+
+const onInit = (reactFlowInstance) => console.log('flow loaded:', reactFlowInstance);
+
+const OverviewFlow = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+  const [menu, setMenu] = useState(null);
+  const ref = useRef(null);
+
+  // we are using a bit of a shortcut here to adjust the edge type
+  // this could also be done with a custom edge for example
+  const edgesWithUpdatedTypes = edges.map((edge) => {
+    // 원래 Example에서는 드롭다운 메뉴로 엣지 유형을 정할 수 있었음
+    // if (edge.sourceHandle) {
+    //   const edgeType = nodes.find((node) => node.type === 'custom').data.selects[edge.sourceHandle];
+    //   edge.type = edgeType;
+    // }
+
+    return edge;
+  });
+
+  const onNodeContextMenu = useCallback(
+    (event, node) => {
+      // Prevent native context menu from showing
+      event.preventDefault();
+
+      // Calculate position of the context menu. We want to make sure it
+      // doesn't get positioned off-screen.
+      const pane = ref.current.getBoundingClientRect();
+      setMenu({
+        id: node.id,
+        top: event.clientY < pane.height - 200 && event.clientY,
+        left: event.clientX < pane.width - 200 && event.clientX,
+        right: event.clientX >= pane.width - 200 && pane.width - event.clientX,
+        bottom: event.clientY >= pane.height - 200 && pane.height - event.clientY,
+      });
+    },
+    [setMenu]
+  );
+  
+  // Close the context menu if it's open whenever the window is clicked.
+  const onPaneClick = useCallback(() => setMenu(null), [setMenu]);
+
+  return (
+    <ReactFlow
+      ref={ref}
+      nodes={nodes}
+      edges={edgesWithUpdatedTypes}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      onPaneClick={onPaneClick}
+      onInit={onInit}
+      fitView
+      fitViewOptions={{nodes: [{id: "0"}]}}
+      attributionPosition="top-right"
+      nodeTypes={nodeTypes}
+      onNodeContextMenu={onNodeContextMenu}
+    >
+      <MiniMap style={minimapStyle} zoomable pannable />
+      <Controls />
+      <Background color="#aaa" gap={16} />
+      {menu && <ContextMenu onClick={onPaneClick} {...menu} />}
+    </ReactFlow>
+  );
+};
+
+export default OverviewFlow;
